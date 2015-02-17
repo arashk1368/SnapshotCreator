fa03f49bbb469ec33a3909f91ad8eb88968d7646
Arash Khodadadi <arashk1368@gmail.com>
2015-02-16 23:37:04 -0500

    Project Created

---
+++ src/main/java/cloudservices/brokerage/crawler/snapshotcreator/App.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 Arash khodadadi.
+ * <http://www.arashkhodadadi.com/>
+ */
+package cloudservices.brokerage.crawler.snapshotcreator;
+
+import cloudservices.brokerage.commons.utils.file_utils.DirectoryUtil;
+import cloudservices.brokerage.commons.utils.logging.LoggerSetup;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Arash Khodadadi <http://www.arashkhodadadi.com/>
+ */
+public class App {
+    private final static Logger LOGGER = Logger.getLogger(App.class.getName());
+    
+    public static void main(String[] args) {
+        createLogFile();
+//        createNewDB();
+    }
+    
+    private static boolean createLogFile() {
+        try {
+            StringBuilder sb = new StringBuilder();
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm");
+            Calendar cal = Calendar.getInstance();
+            sb.append(dateFormat.format(cal.getTime()));
+            String filename = sb.toString();
+            DirectoryUtil.createDir("logs");
+            LoggerSetup.setup("logs/" + filename + ".txt", "logs/" + filename + ".html", Level.FINER);
+            return true;
+        } catch (IOException ex) {
+            LOGGER.log(Level.SEVERE, ex.getMessage(), ex);
+            return false;
+        }
+    }
+}
